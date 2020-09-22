5edd923d445cac74a55dd84570e9f677a4796213
adyashapattnaik123 <pattnaikadyasha99@gmail.com>
2020-09-22 18:58:31 +0530

    Admin and User Classes added

--- onlineBanking/src/main/java/com/lti/entity/Admin.java
+++ onlineBanking/src/main/java/com/lti/entity/Admin.java
@@ -1,5 +1,31 @@
 package com.lti.entity;
 
 public class Admin {
+	
+	private int id;
+	private String password;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
 
 }
--- onlineBanking/src/main/java/com/lti/entity/User.java
+++ onlineBanking/src/main/java/com/lti/entity/User.java
@@ -1,5 +1,32 @@
 package com.lti.entity;
 
 public class User {
+	
+	private int id;
+	//private Customer customer;//fk
+	private String UserPassword;
+	private String TransactionPassword;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUserPassword() {
+		return UserPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		UserPassword = userPassword;
+	}
+	public String getTransactionPassword() {
+		return TransactionPassword;
+	}
+	public void setTransactionPassword(String transactionPassword) {
+		TransactionPassword = transactionPassword;
+	}
+	
+	
 
 }
