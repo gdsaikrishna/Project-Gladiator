b1f9b3ebde6d440621db0cfc6c2027fc2581326e
Soham Bhowmick <sohamslc@gmail.com>
2020-09-22 15:09:05 +0530

    Account and Transaction entity classes done

--- onlineBanking/src/main/java/com/lti/entity/Account.java
+++ onlineBanking/src/main/java/com/lti/entity/Account.java
@@ -1,5 +1,28 @@
 package com.lti.entity;
 
 public class Account {
-
+	
+	private int accountNumber;
+	private User userId; //FK
+	private double balance;
+	
+	public int getAccountNumber() {
+		return accountNumber;
+	}
+	public void setAccountNumber(int accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+	public User getUserId() {
+		return userId;
+	}
+	public void setUserId(User userId) {
+		this.userId = userId;
+	}
+	public double getBalance() {
+		return balance;
+	}
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+	
 }
