97b5f16f4576c1c457b1eecb0487d4f9e2798b53
gdsaikrishna <gdsaikrishna@gmail.com>
2020-09-27 15:39:51 +0530

    backend push

---
+++ banking-backend/src/main/java/com/lti/exception/ServiceException.java
@@ -0,0 +1,37 @@
+package com.lti.exception;
+
+public class ServiceException extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ServiceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
