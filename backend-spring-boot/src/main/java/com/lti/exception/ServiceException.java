8ea5e0d4dba02174e8b963b60ce61816cee9b500
gdsaikrishna <gdsaikrishna@gmail.com>
2020-09-29 18:34:10 +0530

    Spring boot backend push

---
+++ backend-spring-boot/src/main/java/com/lti/exception/ServiceException.java
@@ -0,0 +1,32 @@
+package com.lti.exception;
+
+public class ServiceException extends RuntimeException{
+
+	public ServiceException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ServiceException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+}
