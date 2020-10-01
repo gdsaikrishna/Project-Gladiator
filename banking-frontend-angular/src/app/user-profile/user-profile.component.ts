b6e455dc0670d11bc3ffbbe9948d5c4e394ace2f
gdsaikrishna <gdsaikrishna@gmail.com>
2020-10-01 08:28:05 +0530

    small change in customer model

--- banking-frontend-angular/src/app/user-profile/user-profile.component.ts
+++ banking-frontend-angular/src/app/user-profile/user-profile.component.ts
@@ -1,13 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-import { FormBuilder } from '@angular/forms';
-import { Customer } from '../models/customer';
-
-=======
 
 import { Customer } from './../models/customer';
->>>>>>> abd01a1fe15303ae883e886e69adc9474a84e1fe
 
 @Component({
   selector: 'app-user-profile',
