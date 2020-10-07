515ae7c3d385709461c66ac283bf0276ce344594
shubam98 <subhamsigr8@gmail.com>
2020-10-07 16:22:50 +0530

    Changes in AccountStatement

--- backend-spring-boot/src/main/java/com/lti/service/AccountStatementServiceImpl.java
+++ backend-spring-boot/src/main/java/com/lti/service/AccountStatementServiceImpl.java
@@ -1,10 +1,15 @@
 package com.lti.service;
 
+
+import java.util.ArrayList;
 import java.util.List;
+
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import com.lti.dto.StatementTransactionDto;
 import com.lti.entity.Account;
 import com.lti.entity.Transaction;
 import com.lti.exception.ServiceException;
@@ -15,34 +20,73 @@ import com.lti.repository.TransactionRepository;
 public class AccountStatementServiceImpl implements AccountStatementService{
   @Autowired
   private AccountStatement accountStatement;
-  @Autowired
   private AccountRepository accountRepository;
   
-  public List<Transaction> accountStatment(int userId){
-	  Account acc = (Account) accountRepository.fetchAccountsByUserId( userId);
+  
+  @Override
+  
+  public  List<StatementTransactionDto> accountStatment(int userId){
+	  Account acc = (Account) accountRepository.findAccountByUserId(userId);
 	  try {
-		List<Transaction> transactions = accountStatement.getAllRecords(acc.getAccountNumber());
-		return transactions;
-	} catch (Exception e) {
+		  List<Transaction> transactions= accountStatement.getAllRecords(acc.getAccountNumber());
+		  List<StatementTransactionDto> statementTransactionDto = new ArrayList<>();
+		  for(Transaction t: transactions) {
+			  StatementTransactionDto stateTransaction = new StatementTransactionDto();
+			  stateTransaction.setFromAccountNumber(t.getDebitAccount().getAccountNumber());
+			  stateTransaction.setToAccountNumber(t.getCreditAccount().getAccountNumber());
+			  stateTransaction.setDateTime(t.getTransactionDateTime());
+			  stateTransaction.setTransactionId(t.getTransactionId());
+			  stateTransaction.setRemark(t.getRemarks());
+			  stateTransaction.setTransactionType(t.getTransactionType());
+			  
+			  statementTransactionDto.add(stateTransaction);
+		  }
+		  return statementTransactionDto;
+		  
+		  } catch (Exception e) {
 		// TODO Auto-generated catch block
 		throw new ServiceException("Error occured while fetching transactions");
 	}
 	  
   }
 
+
+
+
 @Override
-public List<Transaction> accountHistory(int userId) {
-	Account acc = (Account) accountRepository.fetchAccountsByUserId(userId);
-	try {
-		List<Transaction> transactions = accountStatement.getAccountHistory(acc.getAccountNumber());
-		return transactions;
-	} catch (Exception e) {
-		throw new ServiceException("Error occured while fetching transactions");
-	}
+public List<StatementTransactionDto> accountHistory(int userId) {
+	 Account acc = (Account) accountRepository.findAccountByUserId(userId);
+	  try {
+		  List<Transaction> transactions= accountStatement.getAccountHistory(acc.getAccountNumber());
+		  List<StatementTransactionDto> statementTransactionDto = new ArrayList<>();
+		  for(Transaction t: transactions) {
+			  StatementTransactionDto stateTransaction = new StatementTransactionDto();
+			  stateTransaction.setFromAccountNumber(t.getDebitAccount().getAccountNumber());
+			  stateTransaction.setToAccountNumber(t.getCreditAccount().getAccountNumber());
+			  stateTransaction.setDateTime(t.getTransactionDateTime());
+			  stateTransaction.setTransactionId(t.getTransactionId());
+			  stateTransaction.setRemark(t.getRemarks());
+			  stateTransaction.setTransactionType(t.getTransactionType());
+			  
+			  statementTransactionDto.add(stateTransaction);
+		  }
+		  return statementTransactionDto;
+		  
+
+	  } catch (Exception e) {
+	// TODO Auto-generated catch block
+	throw new ServiceException("Error occured while fetching transactions");
+	
 }
+}
+}
+	  
+	  
+
+
   
   
-}	 
+
 	  
   
 
